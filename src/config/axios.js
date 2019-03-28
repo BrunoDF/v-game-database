ae740d2c102af3b5ab466fec7834386184667185
Bruno Fachine <bdfachine@gmail.com>
2019-03-28 20:19:44 -0300

    Adds response error interceptor

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,13 +1,13 @@
 import axios from 'axios'
 import axiosDefaults from 'axios/lib/defaults'
 
-import { IGDB_BASE_URL } from './constants';
+import { LOGIN_ROUTE_NAME, IGDB_BASE_URL } from './constants';
 
 import store from '@/config/store'
+import router from '@/config/router'
 
 // Request interceptor
-export const requestAuthInterceptor = axios.interceptors.request.use(
-  config => {
+export const requestAuthInterceptor = axios.interceptors.request.use(config => {
     const token = store.getters['login/token']
 
     if (token)
@@ -17,6 +17,16 @@ export const requestAuthInterceptor = axios.interceptors.request.use(
   },
   error => Promise.reject(error)
 );
+
+// Response interceptor
+export const responseAuthInterceptor = axios.interceptors.response.use(undefined, async (error) => {
+  if (error.response && error.response.status === 401) {
+    await store.dispatch('login/removeToken')
+    router.push({ name: LOGIN_ROUTE_NAME })
+  }
+
+  throw error
+});
 
 // BaseURL
 axiosDefaults.baseURL = IGDB_BASE_URL
