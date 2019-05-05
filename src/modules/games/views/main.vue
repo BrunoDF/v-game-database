8f2ce6850a374c4cbcba15cc24355db3ffb6c1f5
Bruno Fachine <bdfachine@gmail.com>
2019-05-05 10:43:31 -0300

    Fineshes

--- src/modules/games/views/main.vue
+++ src/modules/games/views/main.vue
@@ -1,27 +1,37 @@
 <template>
   <div class="game-main-view">
-    <template v-for="game in games">
-      <gdb-card :game="game" :key="game.id" />
+    <template v-for="platform in platforms">
+      <gdb-platform-wrapper :platform="platform" :key="platform">
+        <gdb-card v-for="game in games[platform]" :game="game" :key="game.id" />
+      </gdb-platform-wrapper>
     </template>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+
 import { IGDB_PLATFORMS } from '@/config/constants'
 
 import GamesService from '@/modules/games/services/games.service'
+import GdbPlatformWrapper from '@/modules/games/components/gdb-platform-wrapper.vue'
 import GdbCard from '@/modules/shared/components/gdb-card.vue'
 
 export default {
   components: {
+    'gdb-platform-wrapper': GdbPlatformWrapper,
     'gdb-card': GdbCard
   },
   async created() {
-    this.games = await GamesService.mostPopularByPlatform(IGDB_PLATFORMS.SWITCH)
+    this.platforms.map(async (platform) => {
+      const result = await GamesService.mostPopularByPlatform(platform)
+      Vue.set(this.games, platform, result)
+    })
   },
   data() {
     return {
-      games: []
+      platforms: Object.values(IGDB_PLATFORMS),
+      games: {}
     }
   }
 }
